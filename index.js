8532f2b6fa0a08d59b53e4d1c63437eb6d4386f3
bun0427 <58924545+bun0427@users.noreply.github.com>
2020-07-12 20:48:52 +0800

    new files

---
+++ index.js
@@ -0,0 +1,8 @@
+var linebot = require('linebot');
+var express = require('express');
+
+var bot = linebot({
+  channelId: "1654113312",
+  channelSecret: "43be84f7c67ba24288d9a0fff4fa318f",
+  channelAccessToken: "4lMN7pkYH7X1PiP9vY1Zj43UzzSx5mpKF1M6fIeuiokuA3K+RR3uiv25+mZrPYsw49o7HiEibqkzKQrqZvqZNBuK9CIQyE4adnuAVuJOe7gaUJZGGSc6d46HJKUvXHHoK1S+wzlgTi+XA4dWskUY0wdB04t89/1O/w1cDnyilFU="
+});
